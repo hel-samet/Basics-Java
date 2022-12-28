8c28f44842c3558be9122e12e0428b38fa202c0c
Hel Samet <helsamet001@gmail.com>
2022-12-28 20:37:30 +0700

    Employee

--- input.java
+++ input.java
@@ -2,11 +2,37 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String[] args) {
-        Scanner st=new Scanner(System.in);
+        String name;
+        String geander;
+        int age, hour;
+        double wage, total;
 
-        String myname="helsamet";
-        System.out.print("Enter name:" );
-        myname=st.next(myname);
+        System.out.println("==================================");
 
+        Scanner st = new Scanner(System.in);
+
+        System.out.print("name=");
+        name = st.next();
+
+        System.out.print("Gender=");
+        geander = st.next();
+
+        System.out.print("age=");
+        age = st.nextInt();
+
+        System.out.print("wage=");
+        wage = st.nextDouble();
+
+        System.out.print("hour=");
+        hour = st.nextInt();
+
+        System.out.println("==========================================");
+
+        total = wage * hour;
+
+        System.out.println("name:" + name + "\n" + "Geader:" + geander + "\n" + "Age:" + age + "\n" + "Wage:" + wage
+                + "\n" + "Hour:" + hour + "\n" + "total:" + total);
+
+        System.out.println("==========================================");
     }
 }
