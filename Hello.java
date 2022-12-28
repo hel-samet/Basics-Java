4fffe08d0410166c91afb4c6553b1d335f505848
Hel Samet <helsamet001@gmail.com>
2022-12-28 18:10:16 +0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@
 //   In the previous chapter, we created a Java file called Main.java, and we used the following code to print "Hello World" to the screen: 
 class hello {
     public static void main(String[] agrs) {
-        System.out.println("Hello java Programming");
+        System.out.println("Hello java Programming!");
     }
 }
