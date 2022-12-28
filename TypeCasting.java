f08b7e69c315b802847eb8593dfffe7df222d921
Hel Samet <helsamet001@gmail.com>
2022-12-28 22:19:24 +0700

    typecasting

--- TypeCasting.java
+++ TypeCasting.java
@@ -8,15 +8,47 @@ public class TypeCasting {
         double b = a;
         System.out.println(a);
         System.out.println(b);
-
+      System.out.println("===========================");
         // 2. Narrowing Casting -> manual
         // Example -> doube-> float -> long -> int -> char -> short -> byte
 
         double d = 13.12;
         int i = (int) d;
-        double l= (double)d;
+        double l = (double) d;
         System.out.println(i);
         System.out.println(l);
+
+        // string -> int -> double -> long -> byte
+        System.out.println("===========================");
+        String s = "3.14";
+        double j = Double.valueOf(s);
+        double sumDoube = j + 3.1;
+        System.out.println("String -> Doube" + "\t" + j);
+        System.out.println("Sum Double" + "\t" + sumDoube);
+        System.out.println("===========================");
+        // String -> Byte
+        String m = "127";
+        byte sbyte = Byte.valueOf(m);
+        System.out.println("String -> Byte" + "\t" + sbyte);
+        System.out.println("===========================");
+        // String -> int
+        String cd = "127";
+        int sbytes = Integer.valueOf(cd);
+        System.out.println("String -> Byte" + "\t" + sbytes);
+        System.out.println("===========================");
+        // String -> long
+        String ab = "127";
+        long slong = Byte.valueOf(ab);
+        System.out.println("String -> Byte" + "\t" + slong);
+
+        System.out.println("===========================");
+       // int , long . short , double, ,...-> string
+
+       long num =120;
+       String stnum= String.valueOf(num);
+       System.out.println(" long -> string"+"\t"+1+1234);
+       System.out.println("===========================");
+
 
     }
 }
