738730643c05906118eabadaa338787039bcdabf
Hel Samet <helsamet001@gmail.com>
2022-12-28 19:51:28 +0700

    Code new

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner st=new Scanner(System.in);
+
+        String myname="helsamet";
+        System.out.print("Enter name:" );
+        myname=st.next(myname);
+
+    }
+}
