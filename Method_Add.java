a35daaa167a3ae371e2ac63fae1aa5fecc0840b7
Hel Samet <helsamet001@gmail.com>
2022-12-29 10:20:43 +0700

    Add_Method for Call

---
+++ Method_Add.java
@@ -0,0 +1,18 @@
+public class Method_Add {
+    public static void main(String[] args) {
+        //Call
+
+        int C=add(10, 20);
+        System.out.println(C);
+    }
+
+    // Add Method
+    public static int add (int a, int b){
+        //Body
+        int sum=0;
+        sum =a+b;
+        return sum;
+
+
+    }
+}
