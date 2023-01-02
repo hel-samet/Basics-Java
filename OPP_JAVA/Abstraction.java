63c9506093f10d199879bf38cba6c0f81e141786
Hel Samet <helsamet001@gmail.com>
2023-01-02 15:30:59 +0700

    Abstraction

---
+++ OPP_JAVA/Abstraction.java
@@ -0,0 +1,9 @@
+package OPP_JAVA;
+
+public class Abstraction {
+    // these are abratract methods 
+    // [visibility] abratract returntype methodsName();
+    // abstract class can have data members
+    // abstract  class can have the constructor
+    // this is a concreate method (normal_methods)
+}
