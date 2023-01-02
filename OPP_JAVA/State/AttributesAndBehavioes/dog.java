c754cf69364e792ab754f3eefb8b0847533fbcd7
Hel Samet <helsamet001@gmail.com>
2023-01-02 15:48:06 +0700

    State and Behaviors

---
+++ OPP_JAVA/State/AttributesAndBehavioes/dog.java
@@ -0,0 +1,21 @@
+package OPP_JAVA.State.AttributesAndBehavioes;
+
+public class dog {
+    // State=> Name Age Color 
+    public String name ="luky";
+    public int Age = 2;
+    public String color ="Blue";
+
+    // Behaviors=> Bark Eat Sleep
+
+    public void Bark(){
+        System.out.println("Vos Vos vos \n");
+
+    }
+    public void Eat(){
+        System.out.println("Pizza \n");
+    }
+    public void sleep(){
+        System.out.println("Good Night \n");
+    }
+}
---
+++ OPP_JAVA/State/AttributesAndBehavioes/main.java
@@ -0,0 +1,13 @@
+package OPP_JAVA.State.AttributesAndBehavioes;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println();
+
+
+        dog Dog1 = new dog();
+        Dog1.Bark();
+        Dog1.Eat();
+        Dog1.sleep();
+    }
+}
